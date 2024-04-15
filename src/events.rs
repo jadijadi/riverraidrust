37ee58c3b4f69d481b4387b1dd231238ea4c7775
jozefkonrad <166939429+jozefkonrad@users.noreply.github.com>
2024-04-15 20:19:10 +0200

    coding style again

--- src/events.rs
+++ src/events.rs
@@ -18,49 +18,51 @@ pub fn handle_pressed_keys(world: &mut World) {
         match key {
             Event::Key(event) => {
                 // I'm reading from keyboard into event
+
+                // ???: in the following five times checking
+                // world.player.status == PlayerStatus::Alive
                 match event.code {
                     KeyCode::Char('w') | KeyCode::Up
                         if world.player.status == PlayerStatus::Alive
-                            && world.player.location.l > 1 =>
-                    {
-                        world.player.location.l -= 1
-                    }
+                        && world.player.location.l > 1
+                        => { world.player.location.l -= 1;
+                        }
                     KeyCode::Char('s') | KeyCode::Down
                         if world.player.status == PlayerStatus::Alive
-                            && world.player.location.l < world.maxl - 1 =>
-                    {
-                        world.player.location.l += 1
-                    }
+                        && world.player.location.l < world.maxl - 1
+                        => { world.player.location.l += 1;
+                        }
                     KeyCode::Char('a') | KeyCode::Left
                         if world.player.status == PlayerStatus::Alive
-                            && world.player.location.c > 1 =>
-                    {
-                        world.player.location.c -= 1
-                    }
+                        && world.player.location.c > 1
+                        => { world.player.location.c -= 1;
+                        }
                     KeyCode::Char('d') | KeyCode::Right
                         if world.player.status == PlayerStatus::Alive
-                            && world.player.location.c < world.maxc - 1 =>
-                    {
-                        world.player.location.c += 1
-                    }
-                    KeyCode::Char('q') => world.player.status = PlayerStatus::Quit,
-                    KeyCode::Char('p') if event.kind == KeyEventKind::Press => {
-                        use crate::WorldStatus::*;
-                        world.status = match world.status {
-                            Fluent => Paused,
-                            Paused => Fluent,
-                        };
-                    }
-                    KeyCode::Char(' ') => {
-                        if world.player.status == PlayerStatus::Alive && world.bullets.is_empty() {
-                            let new_bullet = Bullet::new(
-                                world.player.location.c,
-                                world.player.location.l - 1,
-                                world.maxl / 4,
-                            );
-                            world.bullets.push(new_bullet);
+                        && world.player.location.c < world.maxc - 1
+                        => { world.player.location.c += 1;
                         }
-                    }
+                    KeyCode::Char('q')
+                        => world.player.status = PlayerStatus::Quit,
+                    KeyCode::Char('p')
+                        if event.kind == KeyEventKind::Press
+                        => { use crate::WorldStatus::*;
+                             world.status = match world.status {
+                                 Fluent => Paused,
+                                 Paused => Fluent,
+                             };
+                        }
+                    KeyCode::Char(' ')
+                        => { if world.player.status == PlayerStatus::Alive
+                             && world.bullets.is_empty() {
+                                 let new_bullet = Bullet::new(
+                                     world.player.location.c,
+                                     world.player.location.l - 1,
+                                     world.maxl / 4,
+                                 );
+                                 world.bullets.push(new_bullet);
+                             }
+                        }
                     _ => {}
                 }
             }
